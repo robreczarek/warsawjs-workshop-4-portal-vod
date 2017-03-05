4b19f88526ccc99d2e1817ff744e37069f43a044
Robert Obreczarek <robert@obreczarek.com>
2017-03-05 13:00:14 +0100

    Displaing data from JSON
    The service on the video list now draws data from the json file in the assets
    folder.

--- src/app/components/video-list/video-list.component.ts
+++ src/app/components/video-list/video-list.component.ts
@@ -15,10 +15,12 @@ export class VideoListComponent implements OnInit {
   constructor(private videoService: VideoService) { }
 
   ngOnInit() {
-    this.videos = [
-      { id: 'cos', description: 'jakos 1', title: 'test 1', thumb: '/url.png' },
-      { id: 'cos', description: 'jakos 2', title: 'test 2', thumb: '/url.png' },
-      { id: 'cos', description: 'jakos 3', title: 'test 3', thumb: '/url.png' }
-    ]
+    this.videoService.search().subscribe(videos=>{
+      this.videos = videos
+    })
+  }
+
+  onVideoClick() {
+
   }
 }
--- src/app/services/video.service.ts
+++ src/app/services/video.service.ts
@@ -1,8 +1,58 @@
 import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+import { Observable } from 'rxjs/Rx';
+import { Video } from '../models/video/video';
+import 'rxjs/Rx';
 
 @Injectable()
 export class VideoService {
 
-  constructor() { }
+  constructor(private http: Http) { }
+
+  search(): Observable<Video[]> {
+    return this.http.get('/assets/search.json')
+        .map(res=>{
+          let data = res.json()
+
+          return data.items.map(value=>{
+
+            let el:Video = {
+              id: value.id.videoId,
+              description: value.description,
+              thumb: value.snippet.thumbnails.high.url,
+              title: value.title
+            }
+            return el;
+
+          });
+        })
+  }
 
 }
+
+
+/*import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+import {Observable} from 'rxjs/Rx';
+import { Video } from '../models/video/video';
+
+@Injectable()
+export class VideoService {
+
+  constructor(private http: Http) { }
+
+  search(): Observable<Video[]> {
+    return this.http.get('/assets/search.json')
+      .map(value=>{
+        let el:Video = {
+          id: value.id.videoId,
+          description: value.description,
+          thumb: value.snippet.thumbnails.high.url,
+          title: value.title
+        }
+        return el;
+      })
+  }
+
+}
+*/
