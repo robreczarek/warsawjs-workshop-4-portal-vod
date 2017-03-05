642f9eac4c7686e2ec7a9dd75bdf890c9512d721
Robert Obreczarek <robert@obreczarek.com>
2017-03-05 12:26:28 +0100

    Implementing video list
    Added loop for data in video list component. Some css was broken out into the
    component's css file.

--- src/app/components/video-list/video-list.component.ts
+++ src/app/components/video-list/video-list.component.ts
@@ -10,12 +10,15 @@ import { Video } from '../../models/video/video';
 })
 export class VideoListComponent implements OnInit {
 
-  private list: Video[]
+  private videos: Video[]
 
   constructor(private videoService: VideoService) { }
 
   ngOnInit() {
-
+    this.videos = [
+      { id: 'cos', description: 'jakos 1', title: 'test 1', thumb: '/url.png' },
+      { id: 'cos', description: 'jakos 2', title: 'test 2', thumb: '/url.png' },
+      { id: 'cos', description: 'jakos 3', title: 'test 3', thumb: '/url.png' }
+    ]
   }
-
 }
